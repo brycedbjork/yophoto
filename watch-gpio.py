0c3fd1e3f920b2fa718ef5c6fa7e46e6b2050d93
Bryce Bjork <brycedbjork@gmail.com>
2019-12-12 00:20:16 -0500

    board set

--- watch-gpio.py
+++ watch-gpio.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BOARD)
+
 GPIO.add_event_detect(7, GPIO.RISING)  # add rising edge detection on a channel
 if GPIO.event_detected(7):
     print("event detected")
