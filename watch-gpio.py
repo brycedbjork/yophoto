e21645bb884cb9c0e954aae0c7fcd1d1f278fd3d
Bryce Bjork <brycedbjork@gmail.com>
2019-12-12 01:18:01 -0500

    with sleep

--- watch-gpio.py
+++ watch-gpio.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO
 import socket
+from time import sleep
 
 # s = socket.create_connection("localhost", )
+
+activated = 0
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -10,7 +13,11 @@ GPIO.setup(7, GPIO.IN)
 GPIO.add_event_detect(7, GPIO.BOTH, bouncetime=1000)
 
 while True:
+
   if GPIO.event_detected(7):
-    print(GPIO.input(7))
-    if GPIO.input(7):
-      print("event detected")
+    activated = 1
+    print("activated")
+  else:
+    activated = 0
+
+  sleep(1000)
